7343f9de1feb7d2219f63ada4a67fbdf4e59d316
estivvh <150222625+estivvh@users.noreply.github.com>
2023-11-08 07:17:50 +0100

    Add files via upload

---
+++ tarjeta-digital/script.js
@@ -0,0 +1,7 @@
+document.getElementById('siBtn').addEventListener('click', function() {
+    alert('¡Me haces la persona más feliz del mundo! Te amo también.');
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    alert('Espero que podamos seguir siendo amigos.');
+});
